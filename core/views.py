cc2a2eb1df2b2065b2431ccb50a13d0b20145bf6
danii <denisss19971@rambler.ru>
2024-07-18 20:25:27 +0300

    sigup is not working

---
+++ core/views.py
@@ -0,0 +1,21 @@
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from .forms import SignupForm
+# Create your views here.
+def frontpage(request):
+    return render(request,'core/frontpage.html')
+
+def signup(request):
+    if request.method =='POST':
+        form = SignupForm(request.POST)
+        print('Hello there')
+        if form.is_valid():
+            print('form is valid')
+            user = form.save()
+            login(request,user)
+            return redirect('frontpage')
+        else:
+            raise Exception('Упс')
+    else:
+        form =SignupForm()
+    return render(request, 'core/signup.html', {'form':form})
